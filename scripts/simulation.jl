3895babdb8fd93af10414ee95f4853d31fe48913
danivpv <danivpv@gmail.com>
2022-06-17 14:45:16 -0500

    HiggerN. minor change to naming scheme of data

--- scripts/simulation.jl
+++ scripts/simulation.jl
@@ -20,7 +20,7 @@ x = DataFrame(hcat(map(HIV_sol, 0.0:2:max(HIV_tspan...))...)', ["T", "T_inf", "M
 ### Simulation 1
 
 allparams = Dict(
-    "N" => 50, 
+    "N" => 500, 
     "dB" => 20.0,
     "alg" => ["EKF!", "TikonovEKF!"], 
     "noise" => "gaussian_noise"
@@ -29,18 +29,18 @@ allparams = Dict(
 dicts = dict_list(allparams)
 for d in dicts
     f = makesim(d, x)
-    wsave(datadir("EKFvsTikonovEKF", savename(d, "jld2")), f)
+    wsave(datadir("N=$(d["N"])_EKFvsTikonovEKF", savename(d, "jld2")), f)
 end
 
-EKFvsTikonovEKF = collect_results(datadir("EKFvsTikonovEKF"))
+EKFvsTikonovEKF = collect_results(datadir("N=$(d["N"])_EKFvsTikonovEKF"))
 
-XLSX.writetable(datadir("EKFvsTikonovEKF", "full_table.xlsx"), EKFvsTikonovEKF, overwrite=true)
+XLSX.writetable(datadir("N=$(d["N"])_EKFvsTikonovEKF", "full_table.xlsx"), EKFvsTikonovEKF, overwrite=true)
 
 
 ### Simulation 2
 
 allparams = Dict(
-    "N" => 5, 
+    "N" => 100, 
     "dB" => collect(0:1:20),
     "alg" => "TikonovEKF!",  
     "noise" => "gaussian_noise"
@@ -49,18 +49,18 @@ allparams = Dict(
 dicts = dict_list(allparams)
 for d in dicts
     f = makesim(d, x)
-    wsave(datadir("Robustness", savename(d, "jld2")), f)
+    wsave(datadir("N=$(d["N"])_Robustness", savename(d, "jld2")), f)
 end
 
-Robustness = collect_results(datadir("Robustness"))
+Robustness = collect_results(datadir("N=$(d["N"])_Robustness"))
 
-XLSX.writetable(datadir("Robustness", "full_table.xlsx"), Robustness, overwrite=true)
+XLSX.writetable(datadir("N=$(d["N"])_Robustness", "full_table.xlsx"), Robustness, overwrite=true)
 
 
 ### Simulation 3
 
 allparams = Dict(
-    "N" => 5, 
+    "N" => 100, 
     "dB" => collect(0:1:20),
     "alg" => "TikonovEKF!",     
     "noise" => ["gaussian_noise", "uniform_noise"]
@@ -69,9 +69,9 @@ allparams = Dict(
 dicts = dict_list(allparams)
 for d in dicts
     f = makesim(d, x)
-    wsave(datadir("NoiseTypeComparison", savename(d, "jld2")), f)
+    wsave(datadir("N=$(d["N"])_NoiseTypeComparison", savename(d, "jld2")), f)
 end
 
-NoiseTypeComparison = collect_results(datadir("NoiseTypeComparison"))
+NoiseTypeComparison = collect_results(datadir("N=$(d["N"])_NoiseTypeComparison"))
 
-XLSX.writetable(datadir("NoiseTypeComparison", "full_table.xlsx"), NoiseTypeComparison, overwrite=true)
+XLSX.writetable(datadir("N=$(d["N"])_NoiseTypeComparison", "full_table.xlsx"), NoiseTypeComparison, overwrite=true)
